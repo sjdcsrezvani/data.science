419a982114607f61149942368ca5799ebfdee492
sjdcsrezvani <154724583+sjdcsrezvani@users.noreply.github.com>
2024-04-29 12:06:15 +0330

    Delete ML cheat sheet directory

--- ML cheat sheet/math.py
+++
@@ -1,9 +0,0 @@
-# median is middle value in our data no matter what (values are sorted)
-# if we have even numbers of values we take two value in the middle and take average
-
-# we can handle missing data in sorted values: we can use median
-# outlier is a datapoint that is very different from rest of the datapoints
-# median is 50th percentile of the dataset that means 50% of the datapoints on the left and the other 50% on the right
-
-# the range between 25th percentile and 75th percentile is called IQR or interquartile range
-#
--- ML cheat sheet/pandas1.py
+++
@@ -1,6 +0,0 @@
-# these codes are for jupyter notebook
-# lesson one
-import pandas as pd
-df = pd.read_csv('C:\\Users\\PaRsAfZaR\\Downloads\\weather.csv') # reads a csv file as data frame in df
-df['max tempt'].max()  # prints a maximum number in max tempt column
-df['date'][df['max tempt'] == 51]  # Print dates that the max tempt is 51
